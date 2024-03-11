4d36d76fe0cea6c3450fe0883fac0bcea0cff7be
19shishir <153018169+19shishir@users.noreply.github.com>
2024-03-10 21:13:06 -1200

    let var const and their types
    let var const and their types

---
+++ let var const.js
@@ -0,0 +1,45 @@
+// let var const 
+let name= "shiahir singh"
+const email="singhshshir@gmail.com"
+var rasitrationid="sh2@ty"
+city="lucknow"
+const insa_id="shishir190"
+const twitter_id="@shirtr"
+let state;
+let country=""
+let pm=222
+let number=544464
+let availble=true
+console.log(name);
+console.log(email);
+console.log(rasitrationid);
+console.log(insa_id);
+console.log(twitter_id);
+console.log(state);
+console.log(country);
+console.table([name,email,rasitrationid,city,twitter_id,insa_id,state,country]);
+console.log(typeof email);
+console.log(typeof twitter_id);
+console.log(typeof state);
+console.log( typeof country);
+console.log(typeof pm);
+console.log(typeof availble);
+console.table(typeof[name,email,rasitrationid,city,twitter_id,insa_id,state,country]);
+// notes--
+// perfer not use var 
+// becaause of issu in scope and functional scope
+
+
+// data typeof
+//premitive deta types
+// string=>""
+console.log(typeof "shishir");
+//  number=> random number
+console.log(typeof 22);
+//  boolean-=> true or false
+console.log(typeof true);
+// null=>stadalone  value
+console.log(typeof null);
+//undefined=>
+console.log(typeof undefined);
+// symbol=>unique
