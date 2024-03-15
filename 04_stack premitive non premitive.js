4977d750044e1ce1ad312cf5e546ee5494fb3576
19shishir <153018169+19shishir@users.noreply.github.com>
2024-03-14 17:27:42 -1200

    stack (premitivve) and Rerence (non premitive)

---
+++ 04_stack premitive non premitive.js
@@ -0,0 +1,25 @@
+// stack (premitive) ,Heap(non-premitive)
+let  id="#ftenttd"
+console.log(id);
+let  id2=id
+console.log(id2);
+id2="shishir"
+console.log(id2);
+
+
+let user={
+    id:"sinnghshishir180@gmaill.com",
+    password:"@hyyf"
+}
+console.log(user);
+
+
+let user1=user
+console.log(user1);
+user1.id="google@gmaaill.com"
+console.log(user1);
+console.log(user);
+
+user1.password="354@42"
+console.log(user1);
+console.log(user);
