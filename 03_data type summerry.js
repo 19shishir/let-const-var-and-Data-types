2a4d97db2f6fedadd8ab7c168bad19f66e92c56c
19shishir <153018169+19shishir@users.noreply.github.com>
2024-03-14 15:42:38 -1200

    important

---
+++ 03_data type summerry.js
@@ -0,0 +1,69 @@
+// premitie
+// 7 type : string , Number ,Boolean , null,undefined,Bigint,symbol
+
+
+// const Number=45
+// const speed=12
+// const counnt=10
+// const score=336
+// const userNum=4556
+// console.log(userNum,typeof userNum);
+// //number
+// const score2team=44.5
+// const  value=56.566
+// console.log(score2team,typeof score2team);
+
+// //Boolean
+
+// const emaillogin=false
+// const instaId=true
+// console.log(emaillogin,typeof emaillogin,instaId,typeof instaId);
+
+// //null
+
+// const temprature=null
+// console.log(temprature,typeof temprature);
+
+// // undefined
+// let islogginId
+// //   or
+// const islogginid=undefined
+// console.log(islogginId, typeof islogginId);
+
+// //Bigint
+// const stock=45785186974n
+// const infinityNum=5445668672657n
+// console.log(stock,typeof stock,infinityNum,typeof infinityNum);;
+
+// // symbol
+// const wtspp=Symbol(9935668898)
+// const a=Symbol(4454545)
+// // console.log(wtspp,typeof wtspp,a ,typeof a)
+
+//Non premitive
+// Array,Object,function
+//Array
+
+const userName=["sherynsh","shubham","codejam","shiku"]
+console.log(userName,typeof userName);
+
+const cars = [
+    "Saab",
+    "Volvo",
+    "BMW"
+  ];
+console.log(typeof cars);
+//object
+const dream = {
+    cars1:"Saab",
+      cars2:"Volvo",
+    cars3:"BMW"
+};
+
+console.log(dream,typeof dream);
+
+// function
+const something =function(){
+    console.log("hello world");
+}
+console.log(typeof something);
