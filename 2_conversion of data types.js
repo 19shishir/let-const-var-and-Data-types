01106c08e913db95c2f2538a30e6a22699497f98
19shishir <153018169+19shishir@users.noreply.github.com>
2024-03-14 15:41:00 -1200

    conversion

---
+++ 2_conversion of data types.js
@@ -0,0 +1,123 @@
+// confusion of conversion data types
+let age=20;
+console.log(age);
+console.log(typeof age);
+let converstring=String(age)
+console.log(typeof converstring);
+console.log(converstring);
+
+
+let name="shishir singh";
+console.log(name);
+console.log(typeof name);
+let convert_num=Number(name)
+console.log(typeof convert_num);
+console.log(convert_num);
+
+let namee="shishir singh";
+console.log(name);
+console.log(typeof name);
+let convert_numm=Boolean(name)
+console.log(typeof convert_num);
+console.log(convert_num);
+
+let some=null;
+console.log(some);
+console.log(typeof some);
+let convet_string=Number(some)
+console.log(typeof convet_string);
+console.log(convet_string);
+
+
+let numm=null;
+console.log(numm);
+console.log(typeof numm);
+let convet_stringg=String(numm);
+console.log(typeof convet_stringg);
+console.log(convet_stringg);
+
+
+
+
+
+let nummm=undefined;
+console.log(nummm);
+console.log(typeof nummm);
+let convet_stringgg=Number(nummm)
+console.log(typeof convet_stringgg);
+console.log(convet_stringgg);
+
+// "33" => 33
+// "33xyz" =>NaN
+// true => 1 : false =>0
+
+//1 =>true 0 =>false
+// ""=>false
+//"shishir"=>true
+
+                                               //*********Opration********
+let a = 20;
+    b = 10
+    c = a + b
+    console.log(c);
+
+let value = -20
+let negvalue = -value
+console.log(negvalue);
+
+let name1 = "shishir"
+let lastname = " singh"
+console.log(name1 + lastname);
+
+console.log(4555554+5544544);
+console.log(445-45);
+console.log(458*2);
+console.log(54/5);
+console.log(4555554/554);
+console.log(5**55);
+console.log(444%2);
+
+console.log(54<44);
+console.log(54>21);
+console.log(12==13);
+console.log("1" + 2);
+console.log("1" + 5 + 7);
+console.log(1 + 5 + "7");
+console.log(5+"2");
+console.log("5" + "4");
+console.log("10"===80);
+console.log("5"===5);
+console.log("5"==="5");
+
+console.log(+true);
+console.log(-true);
+console.log(+5);
+
+let score = 12
+score++
+console.log(score);
+
+let speed = 120;
+speed--
+console.log(speed);
+
+let count = 430;
+count++
+console.log(count);
+let counttt = 430;
+++counttt
+console.log(counttt);
+
+
+
+let countt = 100;
+countt--
+console.log(countt);
+
+
+let cont = 100;
+--cont
+console.log(cont);
+
+
+
